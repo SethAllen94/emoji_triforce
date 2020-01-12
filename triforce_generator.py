a0192d97c0164eadb36f2d5e2ca8401d9c2f7118
Seth Allen <46136568+SethAllen94@users.noreply.github.com>
2020-01-12 12:13:58 -0500

    Add files via upload

---
+++ triforce_generator.py
@@ -0,0 +1,33 @@
+from math import ceil
+
+# unicode for smiley face emoji
+smiley_face = ("\U0001f600")
+
+#Welcome message
+print("Welcome to the Emoji Triforce generator!\n"
+          "Please enter the desired height (lines of emojis) of your Triforce.\n"
+          "Odd heights are not possible and are automatically rounded up.\n")
+
+run = True
+while run:
+	try:
+		height = int(input("Please enter the desired height of your Triforce."))
+		height*1
+
+		# Single triangle height
+		st_height = ceil(height/2)
+		
+		# Make Triforce
+		for i in range(st_height):
+			print(" " * (st_height) + " " * (st_height - 1- i) + smiley_face * (i+1))
+		for i in range(st_height):
+			print(" " * (st_height - 1- i) + smiley_face * (i+1) + " " * (st_height - i - 1) + " " * (st_height - 1- i) + smiley_face * (i+1))
+
+		again = input("Would you like to make another? (y/n)")
+		if again == 'n':
+			run = False
+	
+	except ValueError:
+		print("Please enter an integer\n")
+
+	
